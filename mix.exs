dc1abb43f2a32b68817459059a0f9de0c99f77ec
Brian Cardarella <brian@dockyard.com>
2016-11-13 13:22:46 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Courier.Mixfile do
 
   def project do
     [app: :courier,
-     version: "0.0.3",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

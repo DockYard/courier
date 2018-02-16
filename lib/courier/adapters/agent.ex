b0a3addc029035304604be8bd8c08c345565637c
cibernox <miguel.camba@gmail.com>
2018-02-16 16:44:13 +0100

    Stop using Module.add_doc
    It has been deprecated and even worse, removed by accident in 1.6

--- lib/courier/adapters/agent.ex
+++ lib/courier/adapters/agent.ex
@@ -10,50 +10,6 @@ defmodule Courier.Adapters.Agent do
       end
   """
 
-  Module.add_doc(__MODULE__, __ENV__.line + 1, :def, {:deliver, 2}, (quote do: [message, opts]), """
-  Delivers the message by storing in an Agent
-  """)
-
-  Module.add_doc(__MODULE__, __ENV__.line + 1, :def, {:messages, 0}, (quote do: []), """
-  Returns a list of all messages in the Agent
-
-  Returns an empty list if no messages are found
-  """)
-
-  Module.add_doc(__MODULE__, __ENV__.line + 1, :def, {:messages_for, 1}, (quote do: [recipient]), """
-  Returns a list of all messages for the given recipient
-
-      defmodule MockAdapter do
-        use Courier.Adapters.Agent
-      end
-      MockAdapter.start_link([])
-      MockAdapter.messages_for("joe@example.com")
-      [%Mail.Message{...}, %Mail.Message{...}]
-  """)
-
-  Module.add_doc(__MODULE__, __ENV__.line + 1, :def, {:recipients, 0}, (quote do: []), """
-  Unique list of all recipients for all messages
-
-  Will include `BCC` recipients
-
-      defmodule MockAdapter do
-        use Courier.Adapters.Agent
-      end
-      MockAdapter.start_link([])
-      MockAdapter.recipients()
-      ["joe@example.com", {"Brian", "brian@example.com"}]
-  """)
-
-  Module.add_doc(__MODULE__, __ENV__.line + 1, :def, {:clear, 0}, (quote do: []), """
-  Clears all messages
-  """)
-
-  Module.add_doc(__MODULE__, __ENV__.line + 1, :def, {:delete, 1}, (quote do: [message]), """
-  Deletes the specific message
-
-  Returns the list of remaning messages.
-  """ )
-
   defmacro __using__([]) do
     quote do
       use Courier.Storage
@@ -63,17 +19,33 @@ defmodule Courier.Adapters.Agent do
         Agent.start_link(fn -> [] end, name: __MODULE__)
       end
 
+      @doc """
+      Delivers the message by storing in an Agent
+      """
       def deliver(%Mail.Message{} = message, _opts) do
         Agent.update(__MODULE__, fn(messages) -> [message | messages] end)
       end
 
+      @doc """
+      Returns a list of all messages in the Agent
+
+      Returns an empty list if no messages are found
+      """
       def messages() do
         Agent.get(__MODULE__, fn(messages) -> messages end)
       end
 
+      @doc """
+      Clears all messages
+      """
       def clear(),
         do: Agent.update(__MODULE__, fn(_) -> [] end)
 
+      @doc """
+      Deletes the specific message
+
+      Returns the list of remaning messages.
+      """
       def delete(message) do
         Agent.update(__MODULE__, fn(messages) -> List.delete(messages, message) end)
       end
